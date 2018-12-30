007f771a9ad83398d373379746813de21b02c356
Nicole D <nnardella@gmail.com>
2018-12-30 20:15:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,10 +26,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species
+  "SELECT series.title,
   FROM series
   JOIN characters ON characters.series_id = series.id
-  GROUP BY species;"
+  GROUP BY species
+  ORDER BY COUNT(species)
+  ;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
