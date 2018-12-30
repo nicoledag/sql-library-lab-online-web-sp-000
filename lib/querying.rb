3a5e8accb813f7b337d408073dda85cc1eca67f3
Nicole D <nnardella@gmail.com>
2018-12-30 20:32:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,5 +39,5 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, SUM(character_books.character_id)
   FROM characters
   JOIN character_books ON character_books.character_id = characters.id
-  GROUP BY name;"
+  GROUP BY name DESC;"
 end
