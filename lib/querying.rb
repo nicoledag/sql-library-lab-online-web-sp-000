39b01f732020064effdc004afaecf40cbd9c4388
Nicole D <nnardella@gmail.com>
2018-12-30 19:44:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(*)
   FROM characters
   GROUP BY species
-  ORDER BY COUNT(species) DESC;"
+;"
 end
 
 def select_name_and_series_subgenres_of_authors
