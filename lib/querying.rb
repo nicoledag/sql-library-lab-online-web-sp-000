0fe2afe2c0e1825a03dee11a44e51e645ca158ba
Nicole D <nnardella@gmail.com>
2018-12-30 19:58:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name, series.subgenres
+  "SELECT name, series.subgenre_id
   FROM authors
   JOIN series ON authors.id = series.author_id;"
 end
