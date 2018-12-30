c8e9f9adca7fffc98a6edcc0c3540057c033b9f8
Nicole D <nnardella@gmail.com>
2018-12-30 19:55:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,8 +20,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT name, series.subgenres
-  FROM authors;
-  JOIN "
+  FROM authors
+  JOIN ;"
 end
 
 def select_series_title_with_most_human_characters
